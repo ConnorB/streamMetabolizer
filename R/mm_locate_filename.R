4183ea24a5f3ef1fba2a11ae1c9abf77c465c3bb
Connor Brown <connorbrown1996@gmail.com>
2025-06-30 21:19:30 -0500

    Update mm filename to check for stan version
    Check for stan version and use old metabolizer syntax if stan version < 2.26.0

--- R/mm_locate_filename.R
+++ R/mm_locate_filename.R
@@ -9,13 +9,33 @@
 #' @return a file path if the file exists or an error otherwise
 #' @keywords internal
 mm_locate_filename <- function(model_name) {
-  
-  package_dir <- system.file("models", package="streamMetabolizer")
-  package_path <- system.file(paste0("models/", model_name), package="streamMetabolizer")
+  package_dir <- system.file("models", package = "streamMetabolizer")
+  package_path <- file.path(package_dir, model_name)
+  deprecated_path <- file.path(package_dir, "deprecated", model_name)
   other_path <- model_name
   
-  if(file.exists(package_path)) return(package_path)
-  if(file.exists(other_path)) return(other_path)
+  # Get version of either backend, if available
+  stan_version <- tryCatch({
+    if (requireNamespace("cmdstanr", quietly = TRUE) && !is.null(cmdstanr::cmdstan_version(error_on_NA = FALSE))) {
+      as.numeric_version(cmdstanr::cmdstan_version())
+    } else if (requireNamespace("rstan", quietly = TRUE)) {
+      as.numeric_version(rstan::stan_version())
+    } else {
+      NA
+    }
+  }, error = function(e) NA)
   
-  stop("could not locate the model file at ", file.path(package_dir, model_name), " or ", other_path) 
+  # If engine is detected and version < 2.26.0, use deprecated model if available
+  if (!is.na(stan_version) && stan_version < "2.26.0" && file.exists(deprecated_path)) {
+    return(deprecated_path)
+  }
+  
+  # Normal fallback paths
+  if (file.exists(package_path)) return(package_path)
+  if (file.exists(other_path)) return(other_path)
+  # Final fallback: warn if engine is unknown
+  if (is.na(stan_version)) {
+    warning("Neither RStan or CmdStanR Detected")
+  }
+  stop("Could not locate the model file at ", file.path(package_dir, model_name), " or ", other_path)
 }
